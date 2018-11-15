f7a5349c600c0315eadb346030a3e8cf2093e5a3
VitaliiHrinchuk <locopiowp@gmail.com>
2018-11-15 12:57:33 +0200

    Added slides and slider animation

--- src/Components/First-page/First-page.vue
+++ src/Components/First-page/First-page.vue
@@ -1,12 +1,17 @@
 <template lang="html">
   <main>
     <div class="container">
-      <div id="slider" class="slider-bg radius-5px">
-        <div class="slider  shadow radius-5px slider-bg" :style="{ 'backgroundImage': 'url(\'' + image + '\')' }">
-          <i class="slider__arrow fas fa-chevron-circle-left"></i>
-          <h1 class="">Travel and meet new friends with ComMet</h1>
-          <!-- <img src="./sliderbg.jpg" alt=""> -->
-          <i class="slider__arrow fas fa-chevron-circle-right"></i>
+      <!-- :style="{ 'backgroundImage': 'url(\'' + sliderImage + '\')' }" -->
+      <div id="slider" class="radius-5px" >
+        <div class="slider  shadow radius-5px " >
+          <i class="slider__arrow fas fa-chevron-circle-left" v-on:click='prevSlide'></i>
+          <h1 class="">{{sliderTitles[currentSlide-1]}}</h1>
+          <div class="images">
+            <transition-group name="slide" tag="div">
+              <div class="images__item" :style="{ 'backgroundImage': 'url(\'' + image + '\')' }" alt="" v-for="(image,key) in imageList" v-bind:key="key" v-show="currentSlide-1 == key"></div>
+            </transition-group>
+          </div>
+          <i class="slider__arrow fas fa-chevron-circle-right" v-on:click='nextSlide'></i>
         </div>
       </div>
     </div>
@@ -19,29 +24,72 @@
 export default {
   data(){
     return{
-      image: require('../../assets/images/sliderbg.jpg')
+      currentSlide: 1,
+      sliderTitles: [
+        'Travel and meet new friends with ComMet',
+        'Create or search Events and enjoy them with others people',
+        'Use ComMet to meet people in real life instead Socials Networks'
+      ],
+      imageList: [
+        require(`../../assets/images/sliderbg_1.jpg`),
+        require(`../../assets/images/sliderbg_2.jpg`),
+        require(`../../assets/images/sliderbg_3.jpg`)
+      ]
+    }
+  },
+
+  methods: {
+    nextSlide(){
+      if(this.currentSlide == 3){
+        this.currentSlide = 1;
+      } else {
+        this.currentSlide++;
+      }
+    },
+    prevSlide(){
+      if(this.currentSlide == 1){
+        this.currentSlide = 3;
+      } else {
+        this.currentSlide--;
+      }
     }
   }
 }
 </script>
 
 <style lang="css">
-  .slider-bg {
-    /* background-image: url('/assets/images/sliderbg.jpg'); */
-    background-size: cover;
-    background-position: center center;
-  }
+
   .slider{
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0 15px;
-
-    background-color: rgba(0,0,0,.5);
+    background-color: rgba(0,0,0,.6);
     color: #fff;
     height: 500px;
     text-align: center;
     font-size: .8em;
+  }
+  .images{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 5px;
+    z-index: -1;
+  }
+  .images__item{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center center;
+
   }
   .slider__arrow{
     font-size: 30px;
@@ -53,6 +101,17 @@ export default {
   }
   .slider__arrow:hover{
     color: rgba(255,255,255,1);
+  }
+
+  .slide-enter-active {
+    transition: all .4s ease;
+  }
+  .slide-leave-active {
+    transition: all .6s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-enter, .slide-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
   }
 
 </style>
